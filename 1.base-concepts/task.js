ffae8d97cb3e1a731051e1544ec20fc1d3929962
Константин Бережной <kbs9@yandex.ru>
2023-09-04 22:10:25 +0300

    задание 1.1 выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d = Math.pow(b, 2) - 4 * a * c
+    if (d < 0) {
+        return arr
+    } else if (d === 0) {
+        arr.push(-b / (2 * a))
+    } else if (d > 0) {
+        arr.push((-b + Math.sqrt(d) ) / (2 * a)) && arr.push((-b - Math.sqrt(d)) / (2 * a))
+    }
+    return arr
 }
+solveEquation(1,5,4)
+solveEquation(1,2,1)
+solveEquation(1,2,10)
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+// function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-}
+// }

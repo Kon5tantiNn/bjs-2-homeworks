ff264a309599312748933f58d3fb0e64f0ea9cc9
Константин Бережной <kbs9@yandex.ru>
2023-09-05 19:46:22 +0300

    убрана лишняя ветка в условии

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
     let arr = [];
     let d = Math.pow(b, 2) - 4 * a * c
-    if (d < 0) {
-        return arr
-    } else if (d === 0) {
+    if (d === 0) {
         arr.push(-b / (2 * a))
     } else if (d > 0) {
         arr.push((-b + Math.sqrt(d) ) / (2 * a)) && arr.push((-b - Math.sqrt(d)) / (2 * a))

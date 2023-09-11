b655f749f22ccedd053f3eca2170a6f6e0a994a6
Константин Бережной <kbs9@yandex.ru>
2023-09-11 11:47:06 +0300

    решение 3 задачи, функция агрегатор, тест - ок

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -69,7 +69,7 @@ function averageEvenElementsWorker(...arr) {
   if(arr.length === 0){
     return 0;
   } 
-  let evenCollector = 0;
+  let evenCollector = 0; 
    let countEvenelements = 0;
    for (let m = 0; m < arr.length; m += 1){
      if(arr[m] % 2 === 0){
@@ -83,7 +83,17 @@ console.log(averageEvenElementsWorker())
 console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9))
 console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35))
 
-function makeWork (arr, func) {
-
+function makeWork(arrOfArr, func) {
+	const maxWorkerResult = [];
+	for(let m = 0; m < arrOfArr.length; m += 1){
+		maxWorkerResult.push(func(...arrOfArr[m]));
+	}
+	const completeMaxResult = Math.max(...maxWorkerResult)
+	return completeMaxResult;
 }
 const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+
+console.log(makeWork(arr, summElementsWorker))
+console.log(makeWork(arr, differenceMaxMinWorker))
+console.log(makeWork(arr, differenceEvenOddWorker))
+console.log(makeWork(arr, averageEvenElementsWorker))

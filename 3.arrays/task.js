2033e1fe9d9813ab2f3ad3459c23bb92389bb9e6
Константин Бережной <kbs9@yandex.ru>
2023-09-17 15:19:43 +0300

    сравнение массивов тест не пройден

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    const compFn = arr1.every((el, i) => el === arr2[i]);
+    if (arr1.length !== arr2.length){
+        return false;
+    }
+    return console.log(compFn);
 }
+compareArrays([1, 2, 3], [1, 2, 3])
+compareArrays([1, 2], [1, 2, 3])
+compareArrays([1, 2, 3], [3, 2, 1])
+compareArrays([0, 1, 2], [0, 1])
+compareArrays([0, 1], [0, 1, 2])
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])
+
 
 function getUsersNamesInAgeRange(users, gender) {
   

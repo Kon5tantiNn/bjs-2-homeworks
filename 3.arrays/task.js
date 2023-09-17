6fc15d2a5a7b7a7fd767d9bcf0388370f58210a2
Константин Бережной <kbs9@yandex.ru>
2023-09-17 21:51:47 +0300

    исправления для авто-теста

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,18 @@
   function compareArrays(arr1, arr2) {
 	if (arr1.length === arr2.length) {
 		let compFn = arr1.every((el, i) => el === arr2[i]);
-		return console.log(compFn);
+		return compFn;
 	} else {
-		return console.log(false);
+		return false;
 	}
 }
 
-compareArrays([1, 2, 3], [1, 2, 3]);
-compareArrays([1, 2], [1, 2, 3]);
-compareArrays([1, 2, 3], [3, 2, 1]);
-compareArrays([0, 1, 2], [0, 1]);
-compareArrays([0, 1], [0, 1, 2]);
-compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]);
+compareArrays([1, 2, 3], [1, 2, 3])
+compareArrays([1, 2], [1, 2, 3])
+compareArrays([1, 2, 3], [3, 2, 1])
+compareArrays([0, 1, 2], [0, 1])
+compareArrays([0, 1], [0, 1, 2])
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])
 
 
 const people = [

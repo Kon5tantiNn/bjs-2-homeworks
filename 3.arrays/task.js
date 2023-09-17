1db382f03f753190daf8241c81d62da0a998977f
Константин Бережной <kbs9@yandex.ru>
2023-09-17 20:57:18 +0300

    решение 2й задачи с условием если длина равна 0

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -14,6 +14,32 @@ compareArrays([0, 1], [0, 1, 2])
 compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])
 
 
+const people = [
+    {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+    {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+    {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+    {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+    {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+    {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+    {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+    {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+    {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+    {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+    {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+    {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+    {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+    {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+  ]
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(currentGender => currentGender.gender === gender)
+  if (result.length === 0) {
+    return 0;
+  }
+  return result.map(get => get.age).reduce((counter, item) => counter + item) / result.length
 }
+
+console.log(getUsersNamesInAgeRange(people, "мужской"))
+console.log(getUsersNamesInAgeRange(people, "женский"))
+console.log(getUsersNamesInAgeRange([], "женский"))
+console.log(getUsersNamesInAgeRange(people, "Камбердикт Бембербетч"))

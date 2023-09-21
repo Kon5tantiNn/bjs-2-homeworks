fb9d551701c8e68bd761faa6f96d9d94a5f4c7e3
Константин Бережной <kbs9@yandex.ru>
2023-09-21 15:59:26 +0300

    исправления по код стайлу

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,46 +1,46 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName
 }
 
-Student.prototype.addMarks = function (...marks) {
-    if(!this.hasOwnProperty("marks")){
-       return null
-    }
-    this.marks.push(...marks)
+Student.prototype.addMarks = function(...marks) {
+	if (!this.hasOwnProperty("marks")) {
+		return null
+	}
+	this.marks.push(...marks)
 }
 
-Student.prototype.getAverage = function () {
-    if(!this.hasOwnProperty("marks") || this.marks.length === 0){
-        return 0;
-    }
-    return parseFloat((this.marks.reduce((acc, item) => acc + item) / this.marks.length).toFixed(1))
+Student.prototype.getAverage = function() {
+	if (!this.hasOwnProperty("marks") || this.marks.length === 0) {
+		return 0;
+	}
+	return parseFloat((this.marks.reduce((acc, item) => acc + item) / this.marks.length).toFixed(1))
 }
 
-Student.prototype.exclude = function (reason) {
-    delete this.subject
-    delete this.marks
-    this.excluded = reason
+Student.prototype.exclude = function(reason) {
+	delete this.subject
+	delete this.marks
+	this.excluded = reason
 }
 
 let student1 = new Student("Василиса", "женский", 19)
 student1.setSubject("Algebra")
 console.log(student1.subject)
 console.log(student1.getAverage())
-student1.addMarks(4,5,4,5)
+student1.addMarks(4, 5, 4, 5)
 console.log(student1.getAverage())
 console.log(student1)
 let student2 = new Student("Артём", "мужской", 25)
 student2.setSubject("Geometry")
 student2.exclude("прогулы")
 console.log(student2)
-student2.addMarks(4,3,5,4,5)
+student2.addMarks(4, 3, 5, 4, 5)
 console.log(student2)
 student1.exclude("опоздания")
 console.log(student1)
